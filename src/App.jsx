36e4352c3af782bd783d9f86bd7ddbb284dddd19
ayequill <ayequillgh@gmail.com>
2023-02-16 00:05:46 +0000

    added reacy-query for fetching from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { useState, useEffect } from "react";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { useQuery } from "react-query";
+import { nanoid } from "nanoid";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "./sass/App.scss";
 import Homepage from "./components/Homepage";
+import Quiz from "./components/Quiz";
 import axios from "axios";
-
-const queryClient = new QueryClient();
+import {dummyData} from './data.js'
 
 function App() {
-  const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
-    axios
-      .get(
-        "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
-      )
-      .then((res) => res.data)
-  );
-
+  // const { isLoading, error, data, isFetching } = useQuery("repoData", () =>
+  //   axios
+  //     .get(
+  //       "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
+  //     )
+  //     .then((res) => res.data)
+  // );
   // if (isLoading) return 'Loading'
   // if (error) return 'An error has occured:' + error.message
+  const [startGame , setStartGame] = useState(false)
+function startQuiz () {
+  setStartGame(prevState => !prevState)
+  console.log('clicked')
+}
 
   return (
-    <QueryClientProvider client={queryClient}>
       <div className="App">
         <main className="main">
-          <Homepage quiz={data} />
+       {!startGame && <Homepage startQuiz={startQuiz} />}
+       { startGame &&<Quiz quiz={dummyData} /> }
         </main>
       </div>
-    </QueryClientProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </QueryClientProvider>,
 )
